cac2eee9ffb89b579a40a21a86ba464044df2bcb
aberu2 <aberu2@alumni.uv.es>
2021-03-03 12:15:48 +0100

    - Estado de la pagina tras la segunda sesion de laboratorio

--- script.js
+++ script.js
@@ -6,24 +6,20 @@ var icono_sol = document.getElementById("sol");
 var navbar = document.getElementById("navbar");
 
 
-btn_flotante.addEventListener('click', () =>{
-	panel_flotante_contenedor.classList.toggle('visible');
-	let abierto = panel_flotante_contenedor.classList.contains('visible');
-	if(abierto){
-		btn_flotante.innerHTML = "Cerrar Panel";
-	}
-	else{
-		btn_flotante.innerHTML = "Abrir Panel";
-	}
+btn_flotante.addEventListener('click', () => {
+  panel_flotante_contenedor.classList.toggle('visible');
+  let abierto = panel_flotante_contenedor.classList.contains('visible');
+  if (abierto) {
+    btn_flotante.innerHTML = "Cerrar Panel";
+  } else {
+    btn_flotante.innerHTML = "Abrir Panel";
+  }
 });
 
-icono_sol.addEventListener('click', ()=>{
-	navbar.classList.toggle('modo');
-	icono_sol.classList.toggle('fa-sun');
-	icono_sol.classList.toggle('fa-moon');
-	
-	
+icono_sol.addEventListener('click', () => {
+  navbar.classList.toggle('modo');
+  icono_sol.classList.toggle('fa-sun');
+  icono_sol.classList.toggle('fa-moon');
+
+
 });
-
-
-
