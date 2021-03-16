858fa9e3b7f967e7097d99ee6119260cc4aac7b7
AlexBR784 <alexbr784@gmail.com>
2021-03-16 12:19:49 +0100

    Mucho cambio

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const panel_flotante_contenedor = document.querySelector('.panel-flotante-conten
 var icono_sol = document.getElementById("sol");
 var icono_vol = document.getElementById("volumen");
 var navbar = document.getElementById("navbar");
-var audio = document.getElementById("audio")
-
+var audio = document.getElementById("audio");
+var headerTareaAni = document.getElementById("header-tarea");
+var headerTareaSim = document.getElementById("header-sim");
+var videoTareaAni = document.getElementById("video-tarea");
+var videoTareaSim = document.getElementById("video-sim");
+var ej = 1;
 
 btn_flotante.addEventListener('click', () => {
   panel_flotante_contenedor.classList.toggle('visible');
@@ -34,3 +38,61 @@ icono_vol.addEventListener('click', () => {
 
 });
 
+function nextEj(t) {
+  if (ej == 4) {
+    ej = 1;
+  } else
+    ej = parseInt(ej) + 1;
+  changeContent(t);
+}
+
+function antEj(t) {
+  if (ej == 0) {
+    ej = 4;
+  } else
+    ej = parseInt(ej) - 1;
+  changeContent(t);
+}
+
+function changeContent(numTarea) {
+if(numTarea == 0){
+  if (ej == 1) {
+    headerTareaAni.innerHTML = "Tarea 1 de animación";
+   videoTareaAni.src = "https://www.youtube.com/embed/5wXUfif3eQU";
+
+  } else if (ej == 2) {
+    headerTareaAni.innerHTML = "Tarea 2 de animación";
+    videoTareaAni.src = "https://www.youtube.com/embed/5wXUfif3eQU";
+
+  } else if (ej == 3) {
+    headerTareaAni.innerHTML = "Tarea 3 de animación";
+    videoTareaAni.src = "https://www.youtube.com/embed/5wXUfif3eQU";
+
+  } else if (ej == 4) {
+    headerTareaAni.innerHTML = "Tarea 4 de animación";
+    videoTareaAni.src = "https://www.youtube.com/embed/5wXUfif3eQU";
+
+  }
+}else if(numTarea == 1){
+	if (ej == 1) {
+	headerTareaSim.innerHTML = "Tarea 1 de simulacion";
+    videoTareaSim.src = "./imagenes/pea_banner.png";
+  } else if (ej == 2) {
+	headerTareaSim.innerHTML = "Tarea 2 de simulación";
+    videoTareaSim.src = "./imagenes/pea_banner.png";
+  } else if (ej == 3) {
+	headerTareaSim.innerHTML = "Tarea 3 de simulación";
+    videoTareaSim.src = "./imagenes/pea_banner.png";
+  } else if (ej == 4) {
+	headerTareaSim.innerHTML = "Tarea 4 de simulación";
+    videoTareaSim.src = "./imagenes/pea_banner.png";
+  }
+}
+
+}
+
+function navEj(num){
+	ej = num;
+	localStorage.setItem("ejercicio", ej);
+	
+}
